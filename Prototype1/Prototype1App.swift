971d5e275f161a546109ae7d2d3feadada1bf5c8
GSemix <63607553+GSemix@users.noreply.github.com>
2022-08-06 19:38:56 +0300

    Initial Commit

---
+++ Prototype1/Prototype1App.swift
@@ -0,0 +1,17 @@
+//
+//  Prototype1App.swift
+//  Prototype1
+//
+//  Created by Семен Безгин on 06.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct Prototype1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
